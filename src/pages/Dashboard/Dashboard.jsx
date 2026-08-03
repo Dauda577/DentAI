9c92aa6bf1555a1507763eea704ad85290b81a36
chris008123 <aidoochris0081@gmail.com>
2026-08-03 20:38:53 +0000

    Removed error

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
                   loading={diagnosesLoading}
                   pagination={
                     recentDiagnoses
-                      ? { page: RecentDiagnoses.page, pageCount: recentDiagnoses.pageCount, onPageChange: setPage }
+                      ? { page: recentDiagnoses.page, pageCount: recentDiagnoses.pageCount, onPageChange: setPage }
                       : undefined
                   }
                 />
